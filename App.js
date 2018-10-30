4ecccd5859c615d5a2236b54f0b41e4c4e6d2c20
Marcus Pontes <marcusporama@gmail.com>
2018-10-30 18:20:44 -0300

    Projeto finalizado!

--- App.js
+++ App.js
@@ -1,56 +1,61 @@
 import React, {Component} from 'react';
 import { FlatList } from 'react-native';
-
 import ForecastCard from './components/ForecastCard';
 
-constructor(props) {
-  super(props);
 
-  this.state = {
-    latitude = 0,
-    longitude = 0,
-    forecast: [],
-    error: ''
-  };
+export default class App extends React.Component {
 
-  getLocation() {
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			latitude: 0,
+			longitude: 0,
+			forecast: [],
+			error:''
+		};
+	}
 
-    // Get the current position of the user
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState(
-          (prevState) => ({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-          }), => { this.getWeather(); }
-        );
-      },
-        (error) => this.setState({ forecast: error.mensage }),
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
-  }
+	componentDidMount(){
+		// Pega a localização do usuário
+		this.getLocation();
+	}
 
+	getLocation(){
 
-getWeather() {
+		// Pegando a posição atual do usuário
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				this.setState(
+					(prevState) => ({
+					latitude: position.coords.latitude, 
+					longitude: position.coords.longitude
+					}), () => { this.getWeather(); }
+				);
+			},
+			(error) => this.setState({ forecast: error.message }),
+			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+		);
+	}
 
-    // Construct the API url to call
-    let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&units=metric&appid=http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}';
+	getWeather(){
 
-    // Chamando API e mudando o estado
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        this.setState((prevState, props) => ({
-          forecast: data
-        }));
-    })
-  }
-}
+		// Construtor para chamar api
+		let url = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + this.state.latitude + '&lon=' + this.state.longitude + '&units=metric&appid=http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={746bab60bb85261fcb6a4f5732916c10}';
 
-render() {
-  return(
-    <FlatList data={this.state.forecast.list} style={{marginTop20}} keyExtractor={item => item.dt_text} renderItem= {({item}) => <ForecastCard detail={item} location={this.state.forecast.city.name} />} />
-    renderItem={({item}) => <ForecastCard detail={item} location={this.state.forecast.city.name} />}
+		// chamando a url
+		fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			this.setState((prevState, props) => ({
+				forecast: data
+			}));
+		})
+	}
 
-  );
+	render() {
+		return (
+			<FlatList data={this.state.forecast.list} style={{marginTop:20}} keyExtractor={item => item.dt_txt} renderItem={({item}) => <ForecastCard detail={item} location={this.state.forecast.city.name} />} />
+		);
+	}
 }
